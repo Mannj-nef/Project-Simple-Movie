ca8e0ace4b886924bfbafe56b6f968160c23eb16
Manh Quan <manhquan.05012002@gmail.com>
2022-09-17 20:12:12 +0700

    m

--- src/API/config.js
+++ src/API/config.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 const API_MOVIE = process.env.REACT_APP_TMDB;
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = "a4bf361f5cdff62a8cda329a39cc0303";
 
 // https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=
 // https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=
