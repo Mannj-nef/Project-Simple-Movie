c67624ff5fe4294a5a11a1ebdb02797c5bf9dd2c
Manh Quan <manhquan.05012002@gmail.com>
2022-09-18 23:48:35 +0700

    connit

--- src/API/config.js
+++ src/API/config.js
@@ -7,25 +7,12 @@ const API_KEY = process.env.REACT_APP_KEY;
 
 export const tmdb = {
   getMovie: (type, page = 1) =>
-    `${API_MOVIE}/movie/${type}?api_key=a4bf361f5cdff62a8cda329a39cc0303&page=${page}`,
-  getDetailMovie: (id) =>
-    `${API_MOVIE}/movie/${id}?api_key=a4bf361f5cdff62a8cda329a39cc0303`,
-  getMeta: (type, id) =>
-    `${API_MOVIE}/movie/${id}/${type}?api_key=a4bf361f5cdff62a8cda329a39cc0303`,
+    `${API_MOVIE}/movie/${type}?api_key=${API_KEY}&page=${page}`,
+  getDetailMovie: (id) => `${API_MOVIE}/movie/${id}?api_key=${API_KEY}`,
+  getMeta: (type, id) => `${API_MOVIE}/movie/${id}/${type}?api_key=${API_KEY}`,
   searchMovie: (param, page = 1) =>
-    `${API_MOVIE}/search/movie?api_key=a4bf361f5cdff62a8cda329a39cc0303&query=${param}&page=${page}`,
+    `${API_MOVIE}/search/movie?api_key=${API_KEY}&query=${param}&page=${page}`,
 
   img500: (url) => `http://image.tmdb.org/t/p/w500/${url}`,
   imgOriginal: (url) => `https://image.tmdb.org/t/p/original/${url}`,
 };
-// export const tmdb = {
-//   getMovie: (type, page = 1) =>
-//     `${API_MOVIE}/movie/${type}?api_key=${API_KEY}&page=${page}`,
-//   getDetailMovie: (id) => `${API_MOVIE}/movie/${id}?api_key=${API_KEY}`,
-//   getMeta: (type, id) => `${API_MOVIE}/movie/${id}/${type}?api_key=${API_KEY}`,
-//   searchMovie: (param, page = 1) =>
-//     `${API_MOVIE}/search/movie?api_key=${API_KEY}&query=${param}&page=${page}`,
-
-//   img500: (url) => `http://image.tmdb.org/t/p/w500/${url}`,
-//   imgOriginal: (url) => `https://image.tmdb.org/t/p/original/${url}`,
-// };
